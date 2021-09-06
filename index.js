6836103a9992fee617d794f31d3b86552c42b981
pavlvs <pavlvsxavier@gmail.com>
2021-09-06 16:50:08 +0100

    added start, pause and tick methods to timer and added listeners to buttons

--- index.js
+++ index.js
@@ -9,27 +9,45 @@
 // csx⇥ extend a class (ES6)
 // m⇥ method (ES6 syntax)
 
+// fan⇥ anonymous function
+// fn⇥ named function
 // asf⇥ async function
 // af⇥ arrow function (ES6)
+// f⇥ arrow function with body (ES6)
+// fr⇥ arrow function with return (ES6)
 // rp⇥ return Promise (ES6)
 
+// fe⇥ forEach loop
+// map⇥ map function
 
 class Timer {
-    constructor (durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton) {
         this.durationInput = durationInput
         this.startButton = startButton
         this.pauseButton = pauseButton
 
-        this.startButton.addEventListener('click', this.start )
+        this.startButton.addEventListener('click', this.start)
+        this.pauseButton.addEventListener('click', this.pause)
     }
 
-    start () {
-        console.log('Time to start the timer!')
+    start = () => {
+        this.tick()
+        this.interval = setInterval(this.tick, 1000)
+    }
+
+    pause = () => {
+        clearInterval(this.interval)
+    }
+
+    tick = () => {
+        console.log('tick')
     }
 }
 
-const durationInput =  document.querySelector('#duration')
-const startButton =  document.querySelector('#start')
-const pauseButton =  document.querySelector('#pause')
+const durationInput = document.querySelector('#duration')
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
 
 const timer = new Timer(durationInput, startButton, pauseButton)
+
+// timer.start()
